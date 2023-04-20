5c1a53997b45c554a2cb7e526d2cae804d52b8dc
DonWIck32 <nitish2k03@gmail.com>
2023-04-20 18:33:07 +0530

    feat(game): create space shooter game, use quad tree for collision detection

---
+++ old_game.js
@@ -0,0 +1,5 @@
+const homeLink = document.getElementById("home-link");
+homeLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "home.html";
+});
